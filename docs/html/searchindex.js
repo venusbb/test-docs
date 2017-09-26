3313d0a14a7e686302976791c53d53f96d8fae4d
venusbb <venus.bailey@digital.cabinet-office.gov.uk>
2017-09-26 17:54:15 +0100

    start again

--- docs/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Notify-manuals\u2019s documentation!"],titleterms:{document:0,indic:0,manual:0,notifi:0,tabl:0,welcom:0}})
